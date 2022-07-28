1f7ad76c207546d5af40f192fd9e779b9ff11b73
asdf4026 <i1427@naver.com>
2022-07-28 21:52:45 +0900

    0728 화이팅

--- src/main/java/com/kyunghan/todolist/service/todo/TodoService.java
+++ src/main/java/com/kyunghan/todolist/service/todo/TodoService.java
@@ -10,8 +10,7 @@ public interface TodoService {
 	//추가
 	public boolean createTodo(CreateTodoReqDto createTodoReqDto) throws Exception;
 	//조회
-	public List<TodoListRespDto> getTodoList(int page, int contentCount) throws Exception;
-	public List<TodoListRespDto> getImportanceTodoList(int page, int contentCount) throws Exception;
+	public List<TodoListRespDto> getTodoList(String type, int page, int contentCount) throws Exception;
 	//수정
 	public boolean updateTodoComplete(int todoCode) throws Exception;
 	public boolean updateTodoImportance(int todoCode) throws Exception;
